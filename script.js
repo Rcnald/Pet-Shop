5bae0e9dec911f4e52f8f37f59624bda2c241cbb
Rcnald <ronaldomjunior05@gmail.com>
2022-11-17 20:21:10 -0300

    pet shop

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 const openClientSignUp = document.getElementById('clientSignUpButton')
 const clientSignUpModal = document.getElementById('clientSignUpModal')
+
+const openAnimalSignUp = document.getElementById('animalSignUpButton')
+const animalSignUpModal = document.getElementById('animalSignUpModal')
+
 const scrollBar = document.querySelector('body')
 
 const viewPassword = document.getElementById('togglePassword')
 const isTypePassword = document.getElementById("clientPassword");
 
-openClientSignUp.addEventListener('click', openModal)
-
-function openModal(){
-    clientSignUpModal.showModal()
-    clientSignUpModal.classList.add('displayFlex')
-    scrollBar.classList.add('overflowHidden')
-}
+openClientSignUp.addEventListener('click', openModalClient)
+openAnimalSignUp.addEventListener('click', openModalAnimal)
 
 document.onkeydown = function(event){
     if(event.key === 'Escape'){
         clientSignUpModal.classList.remove('displayFlex')
+        animalSignUpModal.classList.remove('displayFlex')
         scrollBar.classList.remove('overflowHidden')
     }
 }
 
+function openModalClient(){
+    openModal(clientSignUpModal)
+}
+
+function openModalAnimal(){
+    openModal(animalSignUpModal)
+}
+
+function openModal(whichModal){
+    whichModal.showModal()
+    whichModal.classList.add('displayFlex')
+    scrollBar.classList.add('overflowHidden')
+}
 
 function showPassword() {
     if (isTypePassword.type === "password") {
@@ -29,4 +42,31 @@ function showPassword() {
     }
 
     viewPassword.classList.toggle("bi-eye")
-  }
+}
+
+// Não sei como funciona mas funciona  
+let dialog = document.getElementsByTagName('dialog')[0];
+  dialog.addEventListener('click', function (event) {
+      let rect = dialog.getBoundingClientRect();
+      let isInDialog=(rect.top <= event.clientY && event.clientY <= rect.top + rect.height
+        && rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
+      if (!isInDialog) {
+          dialog.close();
+          clientSignUpModal.classList.remove('displayFlex')
+          animalSignUpModal.classList.remove('displayFlex')
+          scrollBar.classList.remove('overflowHidden')
+      }
+});
+
+let dialog1 = document.getElementsByTagName('dialog')[1];
+dialog1.addEventListener('click', function (event) {
+    let rect1 = dialog1.getBoundingClientRect();
+    let isInDialog1=(rect1.top <= event.clientY && event.clientY <= rect1.top + rect1.height
+      && rect1.left <= event.clientX && event.clientX <= rect1.left + rect1.width);
+    if (!isInDialog1) {
+        dialog1.close();
+        clientSignUpModal.classList.remove('displayFlex')
+        animalSignUpModal.classList.remove('displayFlex')
+        scrollBar.classList.remove('overflowHidden')
+    }
+});
